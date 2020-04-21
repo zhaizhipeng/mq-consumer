f2d8de289dae9e50822738c5c9fe15f9d263dc29
zhaizhipeng <zhaizhipeng@extracme.com>
2020-04-21 14:05:40 +0800

    RocketMQ 消费者快速入门

---
+++ src/main/java/com/ysdrzp/consumer/constants/RocketMQConstants.java
@@ -0,0 +1,10 @@
+package com.ysdrzp.consumer.constants;
+
+public class RocketMQConstants {
+
+    /**
+     * NAME_SERVER 地址
+     */
+    public static final String NAME_SERVER = "106.14.190.145:9876";
+
+}
---
+++ src/main/java/com/ysdrzp/consumer/rocketmq/quick/Consumer.java
@@ -0,0 +1,41 @@
+package com.ysdrzp.consumer.rocketmq.quick;
+
+import com.ysdrzp.consumer.constants.RocketMQConstants;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+public class Consumer {
+
+    public static void main(String[] args) throws MQClientException {
+
+        // Instantiate with specified consumer group name.
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("quick_consumer_group");
+
+        // Specify name server addresses.
+        consumer.setNamesrvAddr(RocketMQConstants.NAME_SERVER);
+
+        // Subscribe one more more topics to consume.
+        consumer.subscribe("quick_topic", "*");
+        // Register callback to execute on arrival of messages fetched from brokers.
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        //Launch the consumer instance.
+        consumer.start();
+
+        System.out.printf("Consumer Started.%n");
+    }
+
+}
